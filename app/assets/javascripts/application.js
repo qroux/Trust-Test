6b26f77cf2f9b22d8a709e3739b5eaab91493eb5
quentin roux <quentin.roux@hotmail.fr>
2019-10-14 19:53:08 +0200

    javascrit moved to webpack

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-
-document.getElementById("csv-uploader").onchange = function() {
-    document.getElementById("csv-uploader").submit();
-};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,7 @@ import "bootstrap";
 $(".alert" ).fadeOut(5000);
 $(".alert-success" ).fadeOut(5000);
 $(".alert-danger" ).fadeOut(5000);
+
+document.getElementById("csv-uploader").onchange = function() {
+    document.getElementById("csv-uploader").submit();
+};
