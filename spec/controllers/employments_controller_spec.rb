58cafca31a72b9b1f81c1c01e49e918774ded46e
quentin roux <quentin.roux@hotmail.fr>
2019-10-17 18:30:42 +0200

    #index,#show,#import,#enrichment,#destroy_all tested

--- spec/controllers/employments_controller_spec.rb
+++ spec/controllers/employments_controller_spec.rb
@@ -17,8 +17,51 @@ RSpec.describe EmploymentsController, type: :controller do
     end
   end
 
+  describe "POST #import" do
+    before(:each) do
+      # fixture_file_upload instead of fixture_file prevents file conversion to String
+      hash_file = {}
+      @uploaded_file = fixture_file_upload('files/archive.csv', 'text/csv')
+      hash_file['csv'] = @uploaded_file
+
+      post :import, params: { file: hash_file }
+    end
+
+    it "Should import CSV file and Create Employments" do
+      expect(Employment.count).to eq(73)
+    end
+
+    it "Should redirect to index with HTTP 302 status code" do
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "PATCH #enrich" do
+    before(:each) do
+      @e1 = create(:employment)
+    end
+
+    it "Should enrich the right Employment" do
+      patch :enrichment, params: { id: @e1 }
+      expect(@e1.reload.enriched).to eq(true)
+    end
+
+    it "Should redirect to #show" do
+      patch :enrichment, params: { id: @e1 }
+      expect(response).to have_http_status(302)
+    end
+  end
+
   describe "DELETE #destroy_all" do
-    it "Should flush entire DB and redirect to index with HTTP 302 status code" do
+    it "Should flush entire DB" do
+      2.times { create(:employment) }
+      expect(Employment.count).to eq(2)
+
+      delete :destroy_all
+      expect(Employment.count).to eq(0)
+      expect(response).to have_http_status(302)
+    end
+    it "Should redirect to index with HTTP 302 status code" do
       delete :destroy_all
       expect(response).to have_http_status(302)
     end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :employment do
-    year {2014}
-    collectivity {"Commune"}
-    contract_type {"Temps Complet"}
-    position {"Agent administratif"}
-    level {"seconde classe"}
+    year { 2013 }
+    collectivity { "COMMUNE" }
+    contract_type { "TEMPS INCOMPLET" }
+    position { "ACCOMPAGNATEURS DE MUSIQUE DES CONSERVATOIRES" }
+    level { "ACCOMPAGNATEUR MUSICAL CONTRACTUEL CONSERVATOIRES" }
   end
 end
